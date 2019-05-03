a7509328fd3c680f04e15507624d7c428b94c082
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-05-03 15:16:18 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,12 @@ class UsersController < ApplicationController
   end
 
   def index
-    
+    @users = User.where('name LIKE(?)', "%#{params[:name]}%")
+    respond_to do |format|
+      format.html
+      format.json
+
+    end
   end
 
   private
