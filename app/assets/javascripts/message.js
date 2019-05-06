d97ee500303f58df5d3bbf31626a26c7a4448a01
ryutaogiwara <ogiwara.ryuta.0527@icloud.com>
2019-05-06 12:00:30 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,49 +1,53 @@
-$(function () {
-  function buildHTML(message) {
-    var imagehtml = message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`
-    var html = `<div class=message>
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                      ${message.user_name}
-                      </div>
-                      <div class="upper-message__date">
-                      ${message.created_at}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                      <p class="lower-message__content">
-                      ${message.content}
-                      </p>
-                      ${imagehtml}
-                    </div>
-                  </div> `
-    return html;
-  }
+$(document).on('turbolinks:load', function () {
 
-  $('#new_message').on('submit', function (e) {
-    e.preventDefault();
-    var formData = new FormData(this);
-    var href = window.location.href
+  $(function () {
+    function buildHTML(message) {
+      var imagehtml = message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`
+      var html = `<div class="message" data-message-id="${message.id}">
+                      <div class="upper-message" data-id="${message.id}">
+                        <div class="upper-message__user-name">
+                        ${message.user_name}
+                        </div>
+                        <div class="upper-message__date">
+                        ${message.created_at}
+                        </div>
+                      </div>
+                      <div class="lower-message">
+                        <p class="lower-message__content">
+                        ${message.content}
+                        </p>
+                        ${imagehtml}
+                      </div>
+                    </div> `
+      return html;
+    }
 
-    $.ajax({
-      url: href,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+    $('#new_message').on('submit', function (e) {
+      e.preventDefault();
+      var formData = new FormData(this);
+      var href = window.location.href
+
+      $.ajax({
+        url: href,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+        .done(function (data) {
+          var html = buildHTML(data);
+          $('.messages').append(html);
+          $('.form__submit').prop("disabled", false);
+          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
+          $(".new_message")[0].reset();
+          $('.hidden')[0].reset();
+        })
+        .fail(function () {
+          alert('error');
+        })
+      return false;
     })
-      .done(function (data) {
-        var html = buildHTML(data);
-        $('.messages').append(html);
-        $(".form__submit").prop("disabled", false);
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
-        $('.form__message').reset();
-        $('.hidden').reset();
-      })
-      .fail(function () {
-        alert('error');
-      })
-    return false;
-  })
+
+  });
 });
